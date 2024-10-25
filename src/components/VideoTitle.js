202cc94d87465221ec8f945c458efc6414623efe
Your Name <you@example.com>
2024-10-25 17:40:09 +0530

    pay && info button changes

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const VideoTitle = ({title,overview}) => {
   return (
@@ -6,8 +6,10 @@ const VideoTitle = ({title,overview}) => {
       <h1 className='text-6xl font-bold'>{title}</h1>
       <p className='py-6 text-lg w-1/3'>{overview}</p>
       <div>
-        <button className='bg-white text-black p-3 px-9 text-xl  rounded-md hover:bg-opacity-80'>Play</button>
-        <button className='mx-2 bg-gray-600 text-white p-3 px-9 text-xl bg-opacity-50 rounded-md'>More Info</button>
+        <button className='bg-white text-black p-3 px-9 text-xl  rounded-md hover:bg-opacity-80 '>
+        
+        ⏵ Play</button>
+        <button className='mx-2 bg-gray-600 text-white p-3 px-9 text-xl bg-opacity-50 rounded-md'>ⓘ More Info</button>
       </div>
     </div> 
   )
